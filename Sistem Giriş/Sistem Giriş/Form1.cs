d1170d3b379445d7f35a624ed6d066e342408673
kerem7fb <117513389+kerem7fb@users.noreply.github.com>
2024-11-04 17:21:35 +0300

    Add files via upload

---
+++ Sistem Giriş/Sistem Giriş/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistem_Giriş
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string kullanici;
+            kullanici = textBox1.Text;
+            if (kullanici == "Muhammed Kerem Doğmuş")
+            {
+                MessageBox.Show("Sisteme kayıtlısınız");
+            }
+            else 
+            {
+                MessageBox.Show("Sisteme kayıtlı Değilsiniz");
+            }
+        }
+    }
+}
